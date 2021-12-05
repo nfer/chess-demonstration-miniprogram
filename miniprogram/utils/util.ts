bb605115b1ba8006af2c1bb8797f5438c7780f16
Nfer Zhuang <nfer.zhuang@gmail.com>
2021-12-05 18:20:38 +0800

    feat: 绘制棋子

--- miniprogram/utils/util.ts
+++ miniprogram/utils/util.ts
@@ -170,6 +170,79 @@ export const drawChessBackground = (id: string, scale: number) => {
   context.draw();
 };
 
+export const drawChessKeys = (id: string, scale: number, keyInfos: Array<KeyInfo>) => {
+  // 使用 wx.createContext 获取绘图上下文 context
+  const context = wx.createCanvasContext(id);
+
+  // scale
+  context.scale(scale, scale);
+
+  context.font = '40px Georgia';
+
+  keyInfos.forEach(item => {
+    const posX = item.x * LINE_SPACE + START_X;
+    const posY = item.y * LINE_SPACE + START_Y;
+    if (item.type) {
+      context.setStrokeStyle('#000');
+      context.setFillStyle('#f1ffe0');
+      context.beginPath();
+      context.arc(posX, posY, 40, 0, 2 * Math.PI);
+      context.fill();
+      context.stroke();
+      context.beginPath();
+      context.arc(posX, posY, 33, 0, 2 * Math.PI);
+      context.stroke();
+      context.setFillStyle('#000');
+    } else {
+      context.setStrokeStyle('#F00');
+      context.setFillStyle('#f2eff5');
+      context.beginPath();
+      context.arc(posX, posY, 40, 0, 2 * Math.PI);
+      context.fill();
+      context.stroke();
+      context.beginPath();
+      context.arc(posX, posY, 33, 0, 2 * Math.PI);
+      context.stroke();
+      context.setFillStyle('#F00');
+    }
+    context.fillText(item.name, posX - 20, posY + 15);
+  })
+  context.draw();
+};
+
+export interface KeyInfo {
+  key: string;
+  name: string;
+  type: number;
+  x: number;
+  y: number;
+}
+
+// '3kN1b1C/8r/9/3Cr4/9/9/9/9/4p4/5K3 w - - 0 1'
+export const parseFenStr = (str: string) => {
+  const arr = str.split(' ');
+  const lines = arr[0].split('/');
+
+  const output = [] as Array<KeyInfo>;
+
+  lines.forEach((line, y) => {
+    let x = 0;
+    for (let i = 0; i < line.length; i += 1) {
+      const c = line[i];
+      if (c > '0' && c <= '9') {
+        x += Number(c);
+      } else {
+        const name = getTextByChar(c);
+        const type = c > 'a' ? 1 : 0;
+        output.push({ key: c, name, type, x, y });
+        x += 1;
+      }
+    }
+  });
+
+  return output;
+};
+
 const formatNumber = (n: number) => {
   const s = n.toString();
   return s[1] ? s : `0${s}`;
@@ -188,4 +261,24 @@ export const formatTime = (date: Date) => {
     } ${
       [hour, minute, second].map(formatNumber).join(':')}`
   );
+};
+
+export const getTextByChar = (ch: string) => {
+  switch (ch) {
+    case 'k': return '将';
+    case 'K': return '帅';
+    case 'a': return '士';
+    case 'A': return '仕';
+    case 'b': return '象';
+    case 'B': return '相';
+    case 'n': return '马';
+    case 'N': return '马';
+    case 'r': return '车';
+    case 'R': return '车';
+    case 'c': return '炮';
+    case 'C': return '炮';
+    case 'p': return '卒';
+    case 'P': return '兵';
+    default: return '空';
+  }
 };
