7a53a129071770a2a98731034d1c663dbc2c4dc1
Nfer Zhuang <nfer.zhuang@gmail.com>
2021-12-14 08:56:56 +0800

    feat: 实现吃掉棋子

--- miniprogram/pages/learnPage/index.ts
+++ miniprogram/pages/learnPage/index.ts
@@ -15,8 +15,8 @@ Page({
   },
   init() {
     const scale = util.getScale();
-    const keyInfos = util.parseFenStr('3kN1b1C/8r/9/3Cr4/9/9/9/9/4p4/5K3 w - - 0 1');
-    const oriKeyInfos = keyInfos.map(item => ({...item}));
+    const oriKeyInfos = util.parseFenStr('3kN1b1C/8r/9/3Cr4/9/9/9/9/4p4/5K3 w - - 0 1');
+    const keyInfos = oriKeyInfos.map(item => ({...item}));
     this.setData({
       scale,
       oriKeyInfos,
@@ -39,12 +39,43 @@ Page({
     const posX = Math.round(offsetX / 100);
     const posY = Math.round(offsetY / 100);
 
+    // 场景一：点击在空白处
+    if (Math.abs(offsetX - posX * 100) > 30 || Math.abs(offsetY - posY * 100) > 30) {
+      return;
+    }
+
     const key = keyInfos.find(item => item.x === posX && item.y === posY);
+
+    // 场景二：点击在棋子上
     if (key) {
-      console.log('click on key', key);
-      util.drawCursor('cursorCanvas', scale, posX, posY);
-      this.setData({ lastKey: key });
-    } else if (lastKey) {
+      // 1.1 选择棋子
+      if (!lastKey) {
+        this.setData({ lastKey: key });
+        util.drawCursor('cursorCanvas', scale, posX, posY);
+        return;
+      }
+
+      // 1.2 取消选择棋子
+      if (lastKey.x === key.x && lastKey.y === key.y) {
+        this.setData({ lastKey: null });
+        util.clearCursor('cursorCanvas', scale);
+        return
+      }
+
+      //  1.3 吃掉棋子
+      const idx = keyInfos.findIndex(item => item.key === lastKey.key);
+      keyInfos[idx].y = posY;
+      keyInfos[idx].x = posX;
+      const newKeyInfos = keyInfos.filter(item => item.key !== key.key);
+      console.log(newKeyInfos, keyInfos, key, lastKey);
+      this.setData({ keyInfos: newKeyInfos, lastKey: null });
+      util.drawChessKeys('itemCanvas', scale, keyInfos);
+      util.clearCursor('cursorCanvas', scale);
+      return;
+    }
+
+    // 场景三：点击在网格上
+    if (lastKey) {
       const idx = keyInfos.findIndex(item => item.key === lastKey.key);
       keyInfos[idx].y = posY;
       keyInfos[idx].x = posX;
