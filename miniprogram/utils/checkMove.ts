d07a1ba805a4093d5964a18fd39f02abd787a00b
Nfer Zhuang <nfer.zhuang@gmail.com>
2022-01-04 09:37:37 +0800

    feat: add block check for 车/炮

--- miniprogram/utils/checkMove.ts
+++ miniprogram/utils/checkMove.ts
@@ -141,6 +141,39 @@ function checkNBlockMove(keyInfo: KeyInfo, keyInfos: Array<KeyInfo>, x: number, 
   return keyInfos.some(item => item.x === keyX && item.y === keyY);
 }
 
+// 炮
+function checkRBlockMove(keyInfo: KeyInfo, keyInfos: Array<KeyInfo>, x: number, y: number) {
+  let innerKeys = [] as Array<KeyInfo>;
+  if (x === keyInfo.x) {
+    const minY = Math.min(keyInfo.y, y);
+    const maxY = Math.max(keyInfo.y, y);
+    innerKeys = keyInfos.filter(item => item.x === x && (item.y > minY && item.y < maxY));
+  } else {
+    const minX = Math.min(keyInfo.x, x);
+    const maxX = Math.max(keyInfo.x, x);
+    innerKeys = keyInfos.filter(item => item.y === y && (item.x > minX && item.x < maxX));
+  }
+  const targetKey = keyInfos.find(item => item.x === x && item.y === y);
+  if (targetKey) {
+    return innerKeys.length !== 1;
+  } else {
+    return !!innerKeys.length;
+  }
+}
+
+// 车
+function checkCBlockMove(keyInfo: KeyInfo, keyInfos: Array<KeyInfo>, x: number, y: number) {
+  if (x === keyInfo.x) {
+    const minY = Math.min(keyInfo.y, y);
+    const maxY = Math.max(keyInfo.y, y);
+    return keyInfos.some(item => item.x === x && (item.y > minY && item.y < maxY));
+  } else {
+    const minX = Math.min(keyInfo.x, x);
+    const maxX = Math.max(keyInfo.x, x);
+    return keyInfos.some(item => item.y === y && (item.x > minX && item.x < maxX));
+  }
+}
+
 export const checkBlockMove = (keyInfo: KeyInfo, keyInfos: Array<KeyInfo>, x: number, y: number) => {
   switch (keyInfo.key) {
     case 'b':
@@ -149,12 +182,12 @@ export const checkBlockMove = (keyInfo: KeyInfo, keyInfos: Array<KeyInfo>, x: nu
     case 'n':
     case 'N':
       return checkNBlockMove(keyInfo, keyInfos, x, y);
-    // case 'r':
-    // case 'R':
-    //   return checkRMove(keyInfo, keyInfos, x, y);
-    // case 'c':
-    // case 'C':
-    //   return checkCMove(keyInfo, keyInfos, x, y);
+    case 'r':
+    case 'R':
+      return checkRBlockMove(keyInfo, keyInfos, x, y);
+    case 'c':
+    case 'C':
+      return checkCBlockMove(keyInfo, keyInfos, x, y);
   }
   return false;
 };
