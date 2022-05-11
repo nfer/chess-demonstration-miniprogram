d9ceeaed48bb8c1d7b18baf8d09522f6ffc156eb
Nfer Zhuang <nfer.zhuang@gmail.com>
2022-05-12 07:56:38 +0800

    feat: 补全'自出洞来无敌手'所有章节

--- miniprogram/pages/index/data.ts
+++ miniprogram/pages/index/data.ts
@@ -23,6 +23,126 @@ export const books = [
         type: 'chapter',
         name: '自字信手炮第五局'
       },
+      {
+        type: 'chapter',
+        name: '出字列手炮第一局'
+      },
+      {
+        type: 'chapter',
+        name: '出字列手炮第二局'
+      },
+      {
+        type: 'chapter',
+        name: '出字列手炮第三局'
+      },
+      {
+        type: 'chapter',
+        name: '出字列手炮第四局'
+      },
+      {
+        type: 'chapter',
+        name: '出字列手炮第五局'
+      },
+      {
+        type: 'chapter',
+        name: '洞字入手炮第一局'
+      },
+      {
+        type: 'chapter',
+        name: '洞字入手炮第二局'
+      },
+      {
+        type: 'chapter',
+        name: '洞字入手炮第三局'
+      },
+      {
+        type: 'chapter',
+        name: '洞字入手炮第四局'
+      },
+      {
+        type: 'chapter',
+        name: '洞字入手炮第五局'
+      },
+      {
+        type: 'chapter',
+        name: '来字窝心炮第一局'
+      },
+      {
+        type: 'chapter',
+        name: '来字窝心炮第二局'
+      },
+      {
+        type: 'chapter',
+        name: '来字窝心炮第三局'
+      },
+      {
+        type: 'chapter',
+        name: '来字窝心炮第四局'
+      },
+      {
+        type: 'chapter',
+        name: '来字窝心炮第五局'
+      },
+      {
+        type: 'chapter',
+        name: '无字袖手炮第一局'
+      },
+      {
+        type: 'chapter',
+        name: '无字袖手炮第二局'
+      },
+      {
+        type: 'chapter',
+        name: '无字袖手炮第三局'
+      },
+      {
+        type: 'chapter',
+        name: '无字袖手炮第四局'
+      },
+      {
+        type: 'chapter',
+        name: '无字袖手炮第五局'
+      },
+      {
+        type: 'chapter',
+        name: '敌字出手炮第一局'
+      },
+      {
+        type: 'chapter',
+        name: '敌字出手炮第二局'
+      },
+      {
+        type: 'chapter',
+        name: '敌字出手炮第三局'
+      },
+      {
+        type: 'chapter',
+        name: '敌字出手炮第四局'
+      },
+      {
+        type: 'chapter',
+        name: '敌字出手炮第五局'
+      },
+      {
+        type: 'chapter',
+        name: '手字应手炮第一局'
+      },
+      {
+        type: 'chapter',
+        name: '手字应手炮第二局'
+      },
+      {
+        type: 'chapter',
+        name: '手字应手炮第三局'
+      },
+      {
+        type: 'chapter',
+        name: '手字应手炮第四局'
+      },
+      {
+        type: 'chapter',
+        name: '手字应手炮第五局'
+      },
     ]
   },
   {
