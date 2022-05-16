d53aad7623d097ecb5dd92cebcc1e098f2eda5c9
Nfer Zhuang <nfer.zhuang@gmail.com>
2022-05-16 22:33:56 +0800

    feat: 调整books中的id指向

--- miniprogram/data/books.ts
+++ miniprogram/data/books.ts
@@ -9,172 +9,172 @@ export const books = [
         name: '自字信手炮第一局'
       },
       {
-        id: 10001,
+        id: 10002,
         type: 'chapter',
         name: '自字信手炮第二局'
       },
       {
-        id: 10001,
+        id: 10003,
         type: 'chapter',
         name: '自字信手炮第三局'
       },
       {
-        id: 10001,
+        id: 10004,
         type: 'chapter',
         name: '自字信手炮第四局'
       },
       {
-        id: 10001,
+        id: 10005,
         type: 'chapter',
         name: '自字信手炮第五局'
       },
       {
-        id: 10001,
+        id: 10006,
         type: 'chapter',
         name: '出字列手炮第一局'
       },
       {
-        id: 10001,
+        id: 10007,
         type: 'chapter',
         name: '出字列手炮第二局'
       },
       {
-        id: 10001,
+        id: 10008,
         type: 'chapter',
         name: '出字列手炮第三局'
       },
       {
-        id: 10001,
+        id: 10009,
         type: 'chapter',
         name: '出字列手炮第四局'
       },
       {
-        id: 10001,
+        id: 10010,
         type: 'chapter',
         name: '出字列手炮第五局'
       },
       {
-        id: 10001,
+        id: 10011,
         type: 'chapter',
         name: '洞字入手炮第一局'
       },
       {
-        id: 10001,
+        id: 10012,
         type: 'chapter',
         name: '洞字入手炮第二局'
       },
       {
-        id: 10001,
+        id: 10013,
         type: 'chapter',
         name: '洞字入手炮第三局'
       },
       {
-        id: 10001,
+        id: 10014,
         type: 'chapter',
         name: '洞字入手炮第四局'
       },
       {
-        id: 10001,
+        id: 10015,
         type: 'chapter',
         name: '洞字入手炮第五局'
       },
       {
-        id: 10001,
+        id: 10016,
         type: 'chapter',
         name: '来字窝心炮第一局'
       },
       {
-        id: 10001,
+        id: 10017,
         type: 'chapter',
         name: '来字窝心炮第二局'
       },
       {
-        id: 10001,
+        id: 10018,
         type: 'chapter',
         name: '来字窝心炮第三局'
       },
       {
-        id: 10001,
+        id: 10019,
         type: 'chapter',
         name: '来字窝心炮第四局'
       },
       {
-        id: 10001,
+        id: 10020,
         type: 'chapter',
         name: '来字窝心炮第五局'
       },
       {
-        id: 10001,
+        id: 10021,
         type: 'chapter',
         name: '无字袖手炮第一局'
       },
       {
-        id: 10001,
+        id: 10022,
         type: 'chapter',
         name: '无字袖手炮第二局'
       },
       {
-        id: 10001,
+        id: 10023,
         type: 'chapter',
         name: '无字袖手炮第三局'
       },
       {
-        id: 10001,
+        id: 10024,
         type: 'chapter',
         name: '无字袖手炮第四局'
       },
       {
-        id: 10001,
+        id: 10025,
         type: 'chapter',
         name: '无字袖手炮第五局'
       },
       {
-        id: 10001,
+        id: 10026,
         type: 'chapter',
         name: '敌字出手炮第一局'
       },
       {
-        id: 10001,
+        id: 10027,
         type: 'chapter',
         name: '敌字出手炮第二局'
       },
       {
-        id: 10001,
+        id: 10028,
         type: 'chapter',
         name: '敌字出手炮第三局'
       },
       {
-        id: 10001,
+        id: 10029,
         type: 'chapter',
         name: '敌字出手炮第四局'
       },
       {
-        id: 10001,
+        id: 10030,
         type: 'chapter',
         name: '敌字出手炮第五局'
       },
       {
-        id: 10001,
+        id: 10031,
         type: 'chapter',
         name: '手字应手炮第一局'
       },
       {
-        id: 10001,
+        id: 10032,
         type: 'chapter',
         name: '手字应手炮第二局'
       },
       {
-        id: 10001,
+        id: 10033,
         type: 'chapter',
         name: '手字应手炮第三局'
       },
       {
-        id: 10001,
+        id: 10034,
         type: 'chapter',
         name: '手字应手炮第四局'
       },
       {
-        id: 10001,
+        id: 10035,
         type: 'chapter',
         name: '手字应手炮第五局'
       },
