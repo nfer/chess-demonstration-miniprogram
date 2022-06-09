86feed83b5103c343593a36ca29c178fda102f2e
Nfer Zhuang <nfer.zhuang@gmail.com>
2022-06-09 10:21:17 +0800

    feat: 拆分ChessSteps组件

---
+++ miniprogram/components/ChessSteps/ChessSteps.ts
@@ -0,0 +1,32 @@
+// components/ChessSteps.ts
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    steps: {
+      type: Array,
+      value: [],
+    },
+    isError: {
+      type: Boolean,
+      value: false,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  lifetimes: {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+  },
+});
