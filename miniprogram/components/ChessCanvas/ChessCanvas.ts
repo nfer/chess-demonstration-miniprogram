e5635e90e1ac9869c5bcab992662881988fe7169
Nfer Zhuang <nfer.zhuang@gmail.com>
2022-06-10 22:05:24 +0800

    feat: 实现点击棋子并判断位置和类型

--- miniprogram/components/ChessCanvas/ChessCanvas.ts
+++ miniprogram/components/ChessCanvas/ChessCanvas.ts
@@ -1,5 +1,6 @@
 // components/ChessCanvas/ChessCanvas.ts
 import { KeyInfo } from '../../interface';
+import { MARGIN_HORIZONTAL, MARGIN_VERTICAL } from '../../utils/constants';
 import * as util from '../../utils/util';
 
 const CANVAS_ID = 'chessCanvas';
@@ -11,10 +12,14 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    scale: {
+      type: Number,
+      value: 1,
+    },
     keyInfos: {
       type: Array,
       value: [],
-    }
+    },
   },
 
   /**
@@ -66,6 +71,31 @@ Component({
       }
 
       setTimeout(() => this.initWithRetry(keyInfos), 200);
-    }
+    },
+
+    selectItem(e: any) {
+      const { scale, keyInfos } = this.data;
+
+      const offsetX = Math.floor(e.detail.x / scale) - MARGIN_HORIZONTAL;
+      const offsetY = Math.floor(e.detail.y / scale) - MARGIN_VERTICAL;
+      const posX = Math.round(offsetX / 100);
+      const posY = Math.round(offsetY / 100);
+
+      // 场景一：点击在空白处
+      if (Math.abs(offsetX - posX * 100) > 30 || Math.abs(offsetY - posY * 100) > 30) {
+        return;
+      }
+
+      const focuskey = keyInfos.find(item => item.x === posX && item.y === posY);
+
+      // 场景二：点击在棋子上
+      if (focuskey) {
+        console.debug('点击在棋子上', focuskey);
+        return;
+      }
+
+      // 场景三：点击在网格上
+      console.debug('点击在网格上', posX, posY);
+    },
   },
 });
