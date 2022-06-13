c04a883b950439b9d1f674cb9ab79d51766389a9
Nfer Zhuang <nfer.zhuang@gmail.com>
2022-06-13 23:02:34 +0800

    feat: add log module

---
+++ miniprogram/utils/log.ts
@@ -0,0 +1,20 @@
+class Log {
+  i(...args: Array<any>) {
+    console.info.apply(console, Array.prototype.slice.call(args));
+  }
+
+  d(...args: Array<any>) {
+    console.debug.apply(console, Array.prototype.slice.call(args));
+  }
+
+  w(...args: Array<any>) {
+    console.warn.apply(console, Array.prototype.slice.call(args));
+  }
+
+  e(...args: Array<any>) {
+    console.error.apply(console, Array.prototype.slice.call(args));
+  }
+}
+
+export default new Log();
+
