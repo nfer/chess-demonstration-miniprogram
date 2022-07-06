993d9b9c846fbb7265e2e36a08e155d4058a8cf1
Nfer Zhuang <nfer.zhuang@gmail.com>
2022-07-06 23:04:38 +0800

    feat: 实现LoadChessDialog布局

--- miniprogram/components/LoadChessDialog/LoadChessDialog.ts
+++ miniprogram/components/LoadChessDialog/LoadChessDialog.ts
@@ -1,4 +1,8 @@
 // components/LoadChessDialog/LoadChessDialog.ts
+import Log from '../../utils/log';
+
+const TAG = 'LoadChessDialog';
+
 Component({
   /**
    * 组件的属性列表
@@ -18,6 +22,10 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onCloseClick(event: WechatMiniprogram.BaseEvent) {
+      Log.d(TAG, `onCloseClick event:${JSON.stringify(event)}`);
+      const eventOption = { bubbles: true, composed: true, capturePhase: true };
+      this.triggerEvent('onDialogClosed', {}, eventOption);
+    },
   },
 });
