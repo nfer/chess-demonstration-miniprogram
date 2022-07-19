4935e87ed87f8dac783cd2b2ce71dacccf0a4bdc
Nfer Zhuang <nfer.zhuang@gmail.com>
2022-07-19 22:56:34 +0800

    feat: 实现'相'的走棋

--- miniprogram/interface/ChessItem.ts
+++ miniprogram/interface/ChessItem.ts
@@ -167,6 +167,24 @@ export class BChessItem extends ChessItem {
     this.name = 'BChessItem';
   }
 
+  public getDestPos(type: string, range: number): KeyPos {
+    Log.d(this.name, 'getDestPos', type, range, this.pronounceX - range);
+    switch (type) {
+      case '进':
+        return {
+          x: this.x + (this.pronounceX - range) * this.direction,
+          y: this.y - Math.abs(this.pronounceX - range) * this.direction,
+        };
+      case '退':
+        return {
+          x: this.x + (this.pronounceX - range) * this.direction,
+          y: this.y + Math.abs(this.pronounceX - range) * this.direction,
+        };
+      default:
+        return EMPTY_KEYPOS;
+    }
+  }
+
   protected checkPosMove(x: number, y: number): boolean {
     Log.d(this.name, 'checkPosMove', x, y);
     let arr = [];
